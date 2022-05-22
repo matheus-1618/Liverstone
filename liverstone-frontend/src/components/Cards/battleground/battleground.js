3ba2b997a0eb953831b0ccb40ce4d46db9da8f89
matheus-1618 <matheus1207live@gmail.com>
2022-05-22 16:49:50 -0300

    Adiciona transição de batalha e faz sorteio de packs
    Co-authored-by: listerogusuku <listeror@al.insper.edu.br>

--- liverstone-frontend/src/components/Cards/battleground/battleground.js
+++ liverstone-frontend/src/components/Cards/battleground/battleground.js
@@ -6,30 +6,45 @@ import Load from "../loadspinner/loadspinner";
 import { useState,useEffect } from "react";
 import axios from "axios";
 
-
 export default function Battleground(props) {
     const [cards, setCards] = useState([]);
     const [load,SetLoad] = useState(true);
+    const [gif,setGif] = useState(true);
     let { ids } = useParams();
     let cardsId = ids.split("&")
+
+    const wait = () => {
+      setTimeout(() => {
+        setGif(false)
+      }, 2500);
+    };
+
     useEffect(() => {
         axios
           .get("http://localhost:8000/all")
-          .then((res) => {setCards(res.data);SetLoad(false)});
+          .then((res) => {setCards(res.data);SetLoad(false);wait()});
       }, []);
+      
     const card_template = <>
-    <div className="card-container">
-    {cards.map((card) => (
-        cardsId.includes(card.id.toString()) ? 
-        (<img className="cards" src={card.image}/>)
-        : (<noscript></noscript>)
-    ))}
-    </div>
-    </>
+          <div className="card-container">
+          {cards.map((card) => (
+              card.id<4 ? 
+              (<img className="cards" src={card.image}/>)
+              : (<noscript></noscript>)
+          ))}
+          </div>
+          <div className="card-container">
+          {cards.map((card) => (
+              cardsId.includes(card.id.toString()) ? 
+              (<img className="cards" src={card.image}/>)
+              : (<noscript></noscript>)
+          ))}
+          </div>
+          </>
+
   return (
     <main className="App">
-      <Appbar/>
-      {load ? <Load/> : card_template}
+      {gif ? <img className="battleground" src="../../../battle.gif"/> :(load ? (<Load/>) : card_template)}
     </main>
   );
 }
--- liverstone-frontend/src/components/Cards/loja/loja.js
+++ liverstone-frontend/src/components/Cards/loja/loja.js
@@ -7,27 +7,57 @@ import axios from "axios";
 
 
 export default function Loja(props) {
-  return (
-    <main className="App">
+  const [gif,setGif] = useState(false);
+  const [cards, setCards] = useState([]);
+  const [showCard,setShowcard] = useState(false)
+  const wait = () => {
+    setGif(true);
+    setTimeout(() => {
+      setGif(false);
+      setShowcard(true);
+    }, 6000);
+  }
+
+  async function get_card(raridade){
+    await axios
+    .get(`http://localhost:8000/${raridade}`)
+    .then((res) => {setCards(res.data);wait()})
+  }
+
+  const carta_template =( <>
+  {  gif ? 
+      (<img className="pack-oppening" src="../../../pack.gif"/>):
+      (<>
       <Appbar/>
       <div class="loja-container">
         <div className="centered">Bem vindo a loja</div>
         <img className="loja" src="https://bnetcmsus-a.akamaihd.net/cms/gallery/L5HF4DAYACAZ1561588341680.gif"/>
       </div>
       <div className="pack-container">
-        <div className="market">
+        <div onClick={()=>get_card('comum')} className="market">
             <h1 className="label"><GiRank1/>Comum</h1>
             <img className="packs" src="../../comum.gif"/>
         </div>
-        <div className="market">
+        <div onClick={()=>get_card('especial')} className="market">
             <h1 className="label"><GiRank2/>Especial</h1>
             <img className="packs" src="../../especial.gif"/>
         </div>
-        <div className="market">
+        <div onClick={()=>get_card('raro')}  className="market">
             <h1 className="label"><GiRank3/>Raro</h1>
             <img className="packs" src="../../raro.gif"/>
         </div>
-      </div>
+      </div></>)
+      }
+  </>)
+
+  const sorteada_template = (<>
+  <Appbar/>
+  <img src={cards.image}/>
+  </>)
+
+  return (
+    <main className="App">
+      {showCard ? sorteada_template : carta_template}
     </main>
   );
 }
