5ed3deeab770d58d50954aab6bb8df51c3272122
matheus-1618 <matheus1207live@gmail.com>
2022-05-24 14:02:50 -0300

    Adiciona bot de batalha funciona
    Co-authored-by: niveaabreu <niveaadl@al.insper.edu.br>

--- liverstone-frontend/src/components/Cards/battleground/battleground.js
+++ liverstone-frontend/src/components/Cards/battleground/battleground.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./battleground.css";
 import { useParams } from "react-router-dom";
-import Appbar from "../appbar/appbar";
 import Load from "../loadspinner/loadspinner";
 import { useState,useEffect } from "react";
 import axios from "axios";
 
 export default function Battleground(props) {
     const [cards, setCards] = useState([]);
+    const [enemies, setEnemies] = useState([]);
     const [userCard, setUserCard] = useState([]);
     const [enemyCard, setEnemyCard] = useState([]);
     const [texto,setTexto] = useState("Hora de Batalhar");
     const [load,SetLoad] = useState(true);
+    const [enemyTurn,setenemyTurn] = useState(false);
+    const [UserTurn,setuserTurn] = useState(false);
     const [gif,setGif] = useState(true);
     let { ids } = useParams();
     let cardsId = ids.split("&")
@@ -34,11 +36,11 @@ export default function Battleground(props) {
     }
     if (enemyCard.includes(card)){
       setEnemyCard([]);
-  }
-    else{
-      setEnemyCard(enemyCard => [...enemyCard, card])
     }
-}
+      else{
+        setEnemyCard(enemyCard => [...enemyCard, card]);
+      }
+  }
 
     const wait = () => {
       setTimeout(() => {
@@ -46,47 +48,65 @@ export default function Battleground(props) {
       }, 2500);
     };
 
-    const sleep = () => {
+    const sleep = (time) => {
       setTimeout(() => {
         setUserCard([]);
         setEnemyCard([]);
-      }, 1000);
+      }, time);
     };
 
-    function mudaTexto(){
-      if (texto=="Ataque realizado" || texto=="Hora de batalhar"){
-        setTexto("Vez do adversário");
-      }
-      else{
-        setTexto("Vez do adversário");
+    function get_selected(card){
+      if (cardsId.includes(card.id.toString())){
+        return card;
       }
     }
 
-    useEffect(()=>  {if (userCard.length==1 && enemyCard.length==1){mudaTexto();sleep()}} )
+    function bot(){
+      sleep(4000)
+      setTimeout(() => {
+        let random = enemies[Math.floor(Math.random()*enemies.length)];
+        setTexto(random.name)
+        setEnemyCard([random.id])
+      }, 1000);
+      setTimeout(() => {
+        let random1 = cards[Math.floor(Math.random()*cards.length)];
+        setUserCard([random1.id])
+        setTexto(random1.name)
+      }, 2000);
+      setenemyTurn(false);
+      setuserTurn(false);
+      sleep(3000)
+    }
+
+    useEffect(()=>  {if (enemyTurn && UserTurn){sleep(500);bot()}})
 
     useEffect(() => {
         axios
           .get("http://localhost:8000/all")
-          .then((res) => {setCards(res.data);SetLoad(false);wait()});
+          .then((res) => {setCards(res.data.filter(get_selected));SetLoad(false);wait()});
+      }, []);
+
+      useEffect(() => {
+        axios
+          .get("http://localhost:8000/random")
+          .then((res) => {setEnemies(res.data);SetLoad(false)});
       }, []);
       
     const card_template = <>
           <h1 className="battlegroud-letra">{texto}</h1>
           <div className="battleground-card-container">
-          {cards.map((card) => (
-              card.id<4 ? 
-              (enemyCard[0]!=card.id ? (<img onClick={()=>{selectEnemy(card.id)}} className="battleground-card" src={card.image}/>) : 
-              (<img onClick={()=>{selectEnemy(card.id)}} className="battleground-card-enemy" src={card.image}/>))
-              : (<noscript></noscript>)
+          {enemies.map((card) => ( 
+            enemyCard[0]!==card.id ? (<img onClick={()=>{selectEnemy(card.id);setenemyTurn(true)}} className="battleground-card" src={card.image}/>) : 
+            (<img onClick={()=>{selectEnemy(card.id)}} className="battleground-card-enemy" src={card.image}/>) 
           ))}
           </div>
+          
           <h1 className="battlegroud-letra">Vs</h1>
+
           <div className="battleground-card-container">
           {cards.map((card) => (
-              cardsId.includes(card.id.toString()) ? 
-              (userCard[0]!=card.id ? (<img onClick={()=>{selectUser(card.id)}} className="battleground-card" src={card.image}/>) : 
-              (<img onClick={()=>{selectUser(card.id)}} className="battleground-card-user" src={card.image}/>))
-              : (<noscript></noscript>)
+            userCard[0]!==card.id ? (<img onClick={()=>{selectUser(card.id);setuserTurn(true)}} className="battleground-card" src={card.image}/>) : 
+            (<img onClick={()=>{selectUser(card.id)}} className="battleground-card-user" src={card.image}/>)
           ))}
           </div>
           </>
